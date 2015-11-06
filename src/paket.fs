ce6c6b9f34bef8a960d9b42e215a06319d0235d7
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-11-06 23:01:00 +0100

    Basic paket commands

---
+++ src/paket.fs
@@ -0,0 +1,65 @@
+[<ReflectedDefinition>]
+module Ionide.VSCode
+
+open System
+open System.Text.RegularExpressions
+open FunScript
+open FunScript.TypeScript
+open FunScript.TypeScript.fs
+open FunScript.TypeScript.child_process
+open FunScript.TypeScript.vscode
+
+open Ionide
+open Ionide.VSCode
+
+module PaketService =
+    let private location =
+        if Process.isWin () then
+            (VSCode.getPluginPath "ionide-paket") + @"\bin\paket.exe"
+        else
+            (VSCode.getPluginPath "ionide-paket") + @"/bin/paket.exe"
+
+
+    let private bootstrapperLocation =
+        if Globals._process.platform.StartsWith("win") then
+            (VSCode.getPluginPath "ionide-paket") + @"\bin\paket.bootstrapper.exe"
+        else
+            (VSCode.getPluginPath "ionide-paket")+ @"/bin/paket.bootstrapper.exe"
+
+    let private spawnPaket cmd =
+        let outputChannel = window.Globals.getOutputChannel "Paket"
+        outputChannel.clear ()
+        let proc = Process.spawnWithNotification location "mono" cmd outputChannel
+        proc.on("exit",unbox<Function>(fun (code : string) ->
+            if code ="0" then
+                window.Globals.showInformationMessage "Paket completed" |> ignore
+            else
+                window.Globals.showErrorMessage "Paket failed" |> ignore
+        )) |> ignore
+        proc
+
+    let UpdatePaketSilent () = Process.spawn bootstrapperLocation "mono" ""
+    let Init () = "init" |> spawnPaket
+    let Install () = "install" |> spawnPaket
+    let Update () = "update" |> spawnPaket
+    let Outdated () = "outdated" |> spawnPaket
+    let Restore () = "restore" |> spawnPaket
+    let AutoRestoreOn () = "auto-restore on" |> spawnPaket
+    let AutoRestoreOff () = "auto-restore off" |> spawnPaket
+    let ConvertFromNuget () = "convert-from-nuget" |> spawnPaket
+    let Simplify () = "simplify" |> spawnPaket
+
+
+type Paket() =
+    member x.activate(state:obj) =
+        PaketService.UpdatePaketSilent () |> ignore
+        commands.Globals.registerCommand("paket.Init", PaketService.Init |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.Install", PaketService.Install |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.Update", PaketService.Update |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.Outdated", PaketService.Outdated |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.Restore", PaketService.Restore |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.AutoRestoreOn", PaketService.AutoRestoreOn |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.AutoRestoreOff", PaketService.AutoRestoreOff |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.ConvertFromNuget", PaketService.ConvertFromNuget |> unbox<CommandCallback>) |> ignore
+        commands.Globals.registerCommand("paket.Simplify", PaketService.Simplify |> unbox<CommandCallback>) |> ignore
+        ()
---
+++ src/vscode-bindings.fsx
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------------
+// Reference all the TypeScript mappings VS Code package might need
+// --------------------------------------------------------------------------------------
+
+#if INTERACTIVE
+#I "../lib"
+#I "../packages/FunScript/lib/net40"
+
+#r "FunScript.dll"
+#r "FunScript.Interop.dll"
+#r "Ionide.Helpers.dll"
+#r "FunScript.TypeScript.Binding.vscode.dll"
+#r "FunScript.TypeScript.Binding.lib.dll"
+#r "FunScript.TypeScript.Binding.node.dll"
+
+#endif
