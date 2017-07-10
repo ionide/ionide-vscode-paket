b8cde23805b163d9187f1a3b9d962abf2fddb048
Steffen Forkmann <sforkmann@gmail.com>
2017-07-10 09:38:41 +0200

    Add clitool as keyword

--- src/paket.fs
+++ src/paket.fs
@@ -271,7 +271,7 @@ let private createDependenciesProvider () =
                         let concatAndLift = String.concat "\n" >> Promise.lift
                         match tags with
                         | [ _ ] ->
-                            ["nuget"; "git"; "github"; "http"; "gist"; "versions"; "source"; "group"
+                            ["nuget"; "git"; "github"; "http"; "gist"; "clitool"; "versions"; "source"; "group"
                              "references: strict"; "framework:"; "content: none"; "copy_content_to_output_dir: always"
                              "import_targets:"; "copy_local:"; "redirects:"; "strategy:"; "lowest_matching:"; "generate_load_scripts" ]
                             |> concatAndLift
